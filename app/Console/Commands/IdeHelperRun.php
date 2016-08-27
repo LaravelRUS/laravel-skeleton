f94817a6d7ee0e093ec21644ead8f3e39ac02b1c
Андрей Соснов <atehnix@gmail.com>
2016-08-27 21:18:29 +0300

    Several improvements (#3)
    * Update gitignore files
    
    * Wrapper for IdeHelper commands
    
    * List of local providers moved to `config/app.php`
    
    * Wrapper for IdeHelper commands
    
    * Simplify IdeHelper command

---
+++ app/Console/Commands/IdeHelperRun.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Illuminate\Console\Command;
+
+class IdeHelperRun extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ide-helper:run';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate a IDE Helper files if current environment is local.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if (!\App::getProvider(IdeHelperServiceProvider::class)) {
+            $this->info(sprintf('Skipped. IdeHelper not registered for %s environment.', \App::environment()));
+            return;
+        }
+        $this->call('ide-helper:generate');
+        $this->call('ide-helper:meta');
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if ($this->app->environment() === 'local') {
-            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $localProviders = config('app.local_providers', []);
+            foreach ($localProviders as $provider) {
+                $this->app->register($provider);
+            }
         }
     }
 }
