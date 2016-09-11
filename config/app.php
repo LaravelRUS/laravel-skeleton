dc90fcee62bc64127578a302c0f68ba5de6c54b0
jhaoda <jhaoda@gmail.com>
2016-09-11 12:03:53 +0700

    Возвращение APP_URL.

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => 'http://'.env('APP_DOMAIN', 'localhost'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
