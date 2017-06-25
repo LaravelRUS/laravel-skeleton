74ed681efd03497d1e682ec827f340f636defb67
JhaoDa <jhaoda@gmail.com>
2017-06-25 15:58:05 +0700

    Синхронизация с laravel/laravel

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -32,6 +32,6 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => 'logout']);
+        $this->middleware('guest')->except('logout');
     }
 }
